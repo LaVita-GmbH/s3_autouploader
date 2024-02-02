c9a43f25b6bc5857eec2763d389066e276983874
azei <andreas.zeidler@lavita.de>
2024-02-02 09:33:16 +0100

    gitignore and autoformat

--- s3_autoupload.py
+++ s3_autoupload.py
@@ -10,15 +10,17 @@ from watchdog.events import FileSystemEvent
 
 
 class S3Uploader(FileSystemEventHandler):
-    def __init__(self, bucket: str, url: str, key: str, secret: str, basepath: Path) -> None:
+    def __init__(
+        self, bucket: str, url: str, key: str, secret: str, basepath: Path
+    ) -> None:
         super().__init__()
         # create an s3 client and pass in the credentials, url and region
         self.client = boto3.client(
-            's3',
+            "s3",
             aws_access_key_id=key,
             aws_secret_access_key=secret,
             endpoint_url=url,
-            region_name='us-east-1'
+            region_name="us-east-1",
         )
         self.bucket = bucket
         self.basepath = basepath
@@ -42,18 +44,18 @@ class S3Uploader(FileSystemEventHandler):
         # get all the files in the bucket
         file_in_bucket = set()
         response = self.client.list_objects_v2(Bucket=self.bucket)
-        if response['KeyCount'] > 0:
-            for obj in response['Contents']:
-                file_in_bucket.add(obj['Key'])
+        if response["KeyCount"] > 0:
+            for obj in response["Contents"]:
+                file_in_bucket.add(obj["Key"])
 
-            while response['IsTruncated']:
+            while response["IsTruncated"]:
                 response = self.client.list_objects_v2(
                     Bucket=self.bucket,
-                    ContinuationToken=response['NextContinuationToken'],
+                    ContinuationToken=response["NextContinuationToken"],
                 )
 
-                for obj in response['Contents']:
-                    file_in_bucket.add(obj['Key'])
+                for obj in response["Contents"]:
+                    file_in_bucket.add(obj["Key"])
 
         for obj in file_in_bucket:
             if obj not in local_files:
@@ -82,18 +84,20 @@ class S3Uploader(FileSystemEventHandler):
         """Upload a single file to the bucket"""
         try:
             self.client.upload_file(
-                filepath,
-                self.bucket,
-                filepath.relative_to(self.basepath).as_posix()
+                filepath, self.bucket, filepath.relative_to(self.basepath).as_posix()
             )
             logging.info(f"Uploaded {filepath} to {self.bucket}")
 
         except PermissionError as e:
-            if(count > 5):
-                logging.error(f"{filepath} could not be uploaded: {e.with_traceback(None)}")
+            if count > 5:
+                logging.error(
+                    f"{filepath} could not be uploaded: {e.with_traceback(None)}"
+                )
                 return
-            logging.error(f"{filepath} could not be uploaded: {e}\nwaiting {count*5} seconds and trying again...")
-            time.sleep(5*count)
+            logging.error(
+                f"{filepath} could not be uploaded: {e}\nwaiting {count*5} seconds and trying again..."
+            )
+            time.sleep(5 * count)
             self.upload(filepath, count + 1)
 
     def on_created(self, event: FileSystemEvent) -> None:
@@ -139,13 +143,15 @@ class Watcher:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Watch a directory for changes and upload to S3")
-    parser.add_argument('dir', help="Directory to watch")
-    parser.add_argument('bucket', help="S3 bucket name")
-    parser.add_argument('url', help="S3 endpoint url")
-    parser.add_argument('key', help="S3 access key")
-    parser.add_argument('secret', help="S3 secret key")
-    parser.add_argument('--log', help="Log file path", default="s3_autoupload.log")
+    parser = argparse.ArgumentParser(
+        description="Watch a directory for changes and upload to S3"
+    )
+    parser.add_argument("dir", help="Directory to watch")
+    parser.add_argument("bucket", help="S3 bucket name")
+    parser.add_argument("url", help="S3 endpoint url")
+    parser.add_argument("key", help="S3 access key")
+    parser.add_argument("secret", help="S3 secret key")
+    parser.add_argument("--log", help="Log file path", default="s3_autoupload.log")
     args = parser.parse_args()
 
     path = Path(args.dir)
